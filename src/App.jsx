838b8cc23d0096c66ea9281fd894acdea2dbda3a
Pandemonium07 <pandemomium@outlook.fr>
2024-06-20 16:23:01 +0200

    gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
-import React from 'react'; 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/home/Home';
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/home/Home";
 import Films from "./pages/films/Films";
 import Photos from "./pages/photos/Photos";
 import Contact from "./pages/contact/Contact";
 
 const router = createBrowserRouter([
-{
-    path : "/",
-    element : <Home/>,
-},
-{
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/*",
+    element: <Home />,
+  },
+  {
     path: "/films",
     element: <Films />,
   },
   {
     path: "/photos",
-    element: <Photos/>,
+    element: <Photos />,
   },
   {
     path: "/contact",
-    element: <Contact/>,
+    element: <Contact />,
   },
-
-])
-
+]);
 
 const App = () => {
-    return <RouterProvider router={router}/>
+  return <RouterProvider router={router} />;
 };
 
 export default App;
